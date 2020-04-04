49492a00142e3aa681c46c505e8aefee30223ae7
ykwoshia <ykwoshia@126.com>
2020-04-04 23:37:37 +0800

    touch ~/.z if not exits

--- centos8/post-install.sh
+++ centos8/post-install.sh
@@ -56,6 +56,9 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
     rm -rf ~/.zshrc
     ln -s ~/Documents/script/zshrc ~/.zshrc
     ln -s ~/Documents/script/custom ~/.oh-my-zsh/custom
+    if [[ ! -f ~/.z ]]; then
+        touch ~/.z
+    fi
 fi
 
 ### powerline fonts
