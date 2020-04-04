f5e3f7cac62f6bb41ffab53cc7afb170ab29f417
ykwoshia <ykwoshia@126.com>
2020-04-04 22:48:00 +0800

    update post-install use local repo

--- centos8/post-install.sh
+++ centos8/post-install.sh
@@ -1,12 +1,22 @@
 # echo https://ykwoshia:passwd@github.com > ~/.git-credentials
+GIT_REMOTE=http://192.168.199.133/ykwoshia
 mkdir -p ~/Documents
 if [[ ! -d ~/Documents/myvim ]]; then
-echo clone myvim
-git clone https://github.com/ykwoshia/myvim.git ~/Documents/myvim
+    echo clone myvim
+    if [[ -z "$GIT_REMOTE" ]]; then
+        git clone https://github.com/myvim.git ~/Documents/myvim
+    else
+        git clone ${GIT_REMOTE}/myvim.git ~/Documents/myvim
+    fi
 fi
+
 if [[ ! -d ~/Documents/script ]]; then
-echo clone script
-git clone https://github.com/ykwoshia/script.git ~/Documents/script
+    echo clone script
+    if [[ -z "$GIT_REMOTE" ]]; then
+        git clone https://github.com/script.git ~/Documents/script
+    else
+        git clone ${GIT_REMOTE}/script.git ~/Documents/script
+    fi
 fi
 
 
@@ -14,47 +24,58 @@ fi
 
 ### vim
 if [[ ! -d ~/.vim ]]; then
-echo setup myvim
-ln -s ~/Documents/myvim/vimrc ~/.vimrc
-mkdir -p ~/.vim/colors
-cp ~/Documents/myvim/Tomorrow-Night-Eighties.vim ~/.vim/colors
+    echo setup myvim
+    ln -s ~/Documents/myvim/vimrc ~/.vimrc
+    mkdir -p ~/.vim/colors
+    cp ~/Documents/myvim/Tomorrow-Night-Eighties.vim ~/.vim/colors
 
-### tmux
-ln -s ~/Documents/myvim/tmuxconf ~/.tmux.conf
+    ### tmux
+    ln -s ~/Documents/myvim/tmuxconf ~/.tmux.conf
 
-### git
-ln -s ~/Documents/myvim/gitconfig ~/.gitconfig
-mkdir -p ~/.config/git
-ln -s ~/Documents/myvim/gitignore ~/.config/git/ignore
+    ### git
+    ln -s ~/Documents/myvim/gitconfig ~/.gitconfig
+    mkdir -p ~/.config/git
+    ln -s ~/Documents/myvim/gitignore ~/.config/git/ignore
 
-### ctags
-ln -s ~/Documents/myvim/ctags ~/.ctags
+    ### ctags
+    ln -s ~/Documents/myvim/ctags ~/.ctags
 
-### ctags
-ln -s ~/Documents/myvim/flake8 ~/.flake8
+    ### ctags
+    ln -s ~/Documents/myvim/flake8 ~/.flake8
 fi
 
 ### zsh
 if [[ ! -d ~/.oh-my-zsh ]]; then
-echo setup zsh
-git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-rm -rf ~/.oh-my-zsh/custom
-rm -rf ~/.zshrc
-ln -s ~/Documents/script/zshrc ~/.zshrc
-ln -s ~/Documents/script/custom ~/.oh-my-zsh/custom
+    echo setup zsh
+    if [[ -z "$GIT_REMOTE" ]]; then
+        git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    else
+        git clone ${GIT_REMOTE}/oh-my-zsh.git ~/.oh-my-zsh
+    fi
+    rm -rf ~/.oh-my-zsh/custom
+    rm -rf ~/.zshrc
+    ln -s ~/Documents/script/zshrc ~/.zshrc
+    ln -s ~/Documents/script/custom ~/.oh-my-zsh/custom
 fi
 
 ### powerline fonts
-# git clone --depth=1 https://github.com/powerline/powerline-fonts.git
-# cd powerline-fonts
-# ./install.sh
-# cd ~/Documents
+if [[ ! -d ~/Documents/powerline-fonts ]]; then
+    if [[ -z "$GIT_REMOTE" ]]; then
+        git clone --depth=1 https://github.com/powerline/powerline-fonts.git
+    else
+        git clone ${GIT_REMOTE}/powerline-fonts.git ~/Documents/powerline-fonts
+    fi
+~/Documents/powerline-fonts/install.sh
+fi
 
 ### fzf
 if [[ ! -d ~/.fzf ]]; then
-echo setup fzf
-git clone --depth=1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+    echo setup fzf
+    if [[ -z "$GIT_REMOTE" ]]; then
+        git clone --depth=1 https://github.com/junegunn/fzf.git ~/.fzf
+    else
+        git clone ${GIT_REMOTE}/fzf.git ~/.fzf
+# ~/.fzf/install
 fi
 
 ### xfce
@@ -70,17 +91,17 @@ fi
 # google-chrome --proxy-server=socks5://127.0.0.1:1080
 #https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
 ### perlbrew
-if [[ ! -d ~/perl5 ]]; then
-echo setup perlbrew
-curl -L https://raw.githubusercontent.com/gugod/App-perlbrew/master/perlbrew-install | bash
-~/perl5/perlbrew/bin/perlbrew install-cpanm
-fi
+# if [[ ! -d ~/perl5 ]]; then
+# echo setup perlbrew
+# curl -L https://raw.githubusercontent.com/gugod/App-perlbrew/master/perlbrew-install | bash
+# ~/perl5/perlbrew/bin/perlbrew install-cpanm
+# fi
 
 ### pyenv
-if [[ ! -d ~/.pyenv ]]; then
-echo setup pyenv
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-fi
+# if [[ ! -d ~/.pyenv ]]; then
+# echo setup pyenv
+# curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+# fi
 ## pyinstaller require CPython build with --enable-shared
 # dnf install make gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel
 # env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install xxx
